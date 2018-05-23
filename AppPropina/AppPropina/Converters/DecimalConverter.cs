4c14057453dde3fffaf8abdea22d47633fd0b7c5
jopatino <36779409+jopatino@users.noreply.github.com>
2018-05-22 21:30:37 -0500

    Final

---
+++ AppPropina/AppPropina/Converters/DecimalConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppPropina.Converters
+{
+    public class DecimalConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valor = (double)value;
+            return valor.ToString("C");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valor = (string)value;
+            return valor.Remove(0);
+        }
+
+    }
+}
